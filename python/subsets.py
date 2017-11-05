0e9d544344a406d7983e184b1cf9dd76a4a0aef2
russelu <30449569+russelu@users.noreply.github.com>
2017-11-05 15:44:02 -0800

    Create subsets.py

---
+++ python/subsets.py
@@ -0,0 +1,19 @@
+class Solution:
+    
+    """
+    @param: nums: A set of numbers
+    @return: A list of lists
+    """
+    
+    def findSubsets(self, nums, subset, startIndex):
+        self.results.append(subset)
+        i = startIndex
+        while i <  len(nums):
+            self.findSubsets(nums, subset + [nums[i]], i+1)
+            i += 1
+            
+    def subsets(self, nums):
+        # write your code here
+        self.results = []
+        self.findSubsets(sorted(nums), [], 0)
+        return self.results
